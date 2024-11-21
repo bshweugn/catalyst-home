7b73628e5ab0e06b6f7e0efb030b3619d6e5c363
Local Piper <artsor53@gmail.com>
2024-11-21 13:54:10 +0300

    fix: added missing user repository

---
+++ backend/src/main/java/itmo/localpiper/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package itmo.localpiper.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import itmo.localpiper.backend.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<Long, User> {
+    
+}
