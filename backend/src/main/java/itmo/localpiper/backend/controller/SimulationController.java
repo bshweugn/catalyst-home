145c83899b42997d26a173aeffb6edd7ee35ca2c
Local Piper <artsor53@gmail.com>
2024-11-23 16:09:52 +0300

    chore: added simulation controller template

---
+++ backend/src/main/java/itmo/localpiper/backend/controller/SimulationController.java
@@ -0,0 +1,28 @@
+package itmo.localpiper.backend.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/simul")
+public class SimulationController {
+    
+    @PostMapping("/executeAction")
+    public String executeAction(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+
+    // GUESS WHO'S BACK, BACK AGAIN
+    @PostMapping("/executeScript")
+    public String executeScript(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
+}
