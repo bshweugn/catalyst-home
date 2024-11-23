4863ff8e586b6bbf19a2c13e975fda02786fd6f7
Local Piper <artsor53@gmail.com>
2024-11-23 19:48:57 +0300

    feat: new command

---
+++ backend/src/main/java/itmo/localpiper/backend/command/commands/PickUpRbCommand.java
@@ -0,0 +1,13 @@
+package itmo.localpiper.backend.command.commands;
+
+import itmo.localpiper.backend.command.Command;
+
+public class PickUpRbCommand implements Command {
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
