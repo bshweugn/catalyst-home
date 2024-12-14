579f1928696cf113a357f9a2cd0611e30d4b765f
Local Piper <artsor53@gmail.com>
2024-12-14 12:20:53 +0300

    feat: users by house/houses by user

--- backend/src/main/java/itmo/localpiper/backend/controller/usermode/LocalDataFetchController.java
+++ backend/src/main/java/itmo/localpiper/backend/controller/usermode/LocalDataFetchController.java
@@ -1,5 +1,7 @@
 package itmo.localpiper.backend.controller.usermode;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,12 @@ import itmo.localpiper.backend.repository.UserRepository;
 import itmo.localpiper.backend.util.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
+import itmo.localpiper.backend.model.House;
+import itmo.localpiper.backend.repository.UserHouseRelRepository;
+
+
 
 @RestController
 @RequestMapping("/api/local")
@@ -21,6 +29,9 @@ public class LocalDataFetchController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserHouseRelRepository userHouseRelRepository;
     
     @GetMapping("/getUserData")
     public ResponseEntity<User> getUserData(HttpServletRequest servletRequest) {
@@ -29,5 +40,27 @@ public class LocalDataFetchController {
             jwtService.extractUsername(
             servletRequest.getHeader("Authorization").substring(7))).get());
     }
+
+    @GetMapping("/getUsersByHouse")
+    public ResponseEntity<List<User>> getUsersByHouse(@RequestParam Long houseId) {
+        return ResponseEntity.ok(
+            userHouseRelRepository.findAllUsersByHouse(houseId)
+        );
+    }
+
+    @GetMapping("/getHousesByUser")
+    public ResponseEntity<List<House>> getMethodName(HttpServletRequest servletRequest) {
+        return ResponseEntity.ok(
+            userHouseRelRepository.findAllHousesByUser(
+                userRepository.findByEmail(
+                    jwtService.extractUsername(
+                        servletRequest.getHeader("Authorization").substring(7)
+                    )
+                ).get().getId()
+            )
+        );
+    }
+    
+    
     
 }
