103a5c83c742e1d620b3bc4fb603ee0e857df41e
Local Piper <artsor53@gmail.com>
2024-12-18 00:09:03 +0300

    fix: delete

--- backend/src/main/java/itmo/localpiper/backend/controller/DeviceImportController.java
+++ backend/src/main/java/itmo/localpiper/backend/controller/DeviceImportController.java
@@ -3,7 +3,6 @@ package itmo.localpiper.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,7 +53,7 @@ public class DeviceImportController {
         return ResponseEntity.ok(importCreateProcessorService.process(crutch));
     }
 
-    @DeleteMapping("/deleteDevice")
+    @PostMapping("/deleteDevice")
     public ResponseEntity<OperationResultResponse> deleteDevice(@Valid @RequestBody DeviceDeleteRequest request,
         HttpServletRequest servletRequest) {
             String token = servletRequest.getHeader("Authorization").substring(7);
