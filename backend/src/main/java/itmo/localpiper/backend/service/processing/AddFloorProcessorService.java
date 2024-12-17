b37ec05ada83617f35c4b5a9edaf10fde1b36e23
Local Piper <artsor53@gmail.com>
2024-12-17 22:56:11 +0300

    feat: role check

--- backend/src/main/java/itmo/localpiper/backend/service/processing/AddFloorProcessorService.java
+++ backend/src/main/java/itmo/localpiper/backend/service/processing/AddFloorProcessorService.java
@@ -1,5 +1,7 @@
 package itmo.localpiper.backend.service.processing;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
@@ -7,11 +9,17 @@ import org.springframework.stereotype.Service;
 import itmo.localpiper.backend.dto.request.homeutils.AddFloorRequest;
 import itmo.localpiper.backend.dto.response.HoldableResultResponse;
 import itmo.localpiper.backend.dto.response.OperationResultResponse;
+import itmo.localpiper.backend.exceptions.RoleViolationException;
 import itmo.localpiper.backend.model.Floor;
+import itmo.localpiper.backend.model.House;
 import itmo.localpiper.backend.model.User;
+import itmo.localpiper.backend.model.UserHouseRel;
+import itmo.localpiper.backend.repository.HouseRepository;
+import itmo.localpiper.backend.repository.UserHouseRelRepository;
 import itmo.localpiper.backend.repository.UserRepository;
 import itmo.localpiper.backend.service.entity.FloorService;
 import itmo.localpiper.backend.service.transactional.TransactionalCreateRecursiveEntityService;
+import itmo.localpiper.backend.util.enums.HouseOwnership;
 import itmo.localpiper.backend.util.enums.ProcessingStatus;
 
 @Service
@@ -24,6 +32,12 @@ public class AddFloorProcessorService extends AbstractProcessor<Pair<String,AddF
     private UserRepository userRepository;
 
     @Autowired
+    private HouseRepository houseRepository;
+
+    @Autowired
+    private UserHouseRelRepository uhrRepository;
+
+    @Autowired
     private TransactionalCreateRecursiveEntityService tcres;
 
     @Override
@@ -31,10 +45,13 @@ public class AddFloorProcessorService extends AbstractProcessor<Pair<String,AddF
         String email = request.getFirst();
         String name = request.getSecond().getName();
         Long houseId = request.getSecond().getHouseId();
+        User user = userRepository.findByEmail(email).get();
         if (houseId == null) {
-            User user = userRepository.findByEmail(email).get();
             return tcres.createFloor(user, name);
         }
+        House house = houseRepository.findById(houseId).get();
+        Optional<UserHouseRel> maybeUhr = uhrRepository.findByUserAndHouse(user, house);
+        if (maybeUhr.isEmpty() || maybeUhr.get().getRole() == HouseOwnership.GUEST) throw new RoleViolationException("Can't add floor - permission denied");
         return floorService.create(name, houseId);
     }
 
--- backend/src/main/java/itmo/localpiper/backend/service/processing/AddRoomProcessorService.java
+++ backend/src/main/java/itmo/localpiper/backend/service/processing/AddRoomProcessorService.java
@@ -1,5 +1,7 @@
 package itmo.localpiper.backend.service.processing;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
@@ -7,11 +9,17 @@ import org.springframework.stereotype.Service;
 import itmo.localpiper.backend.dto.request.homeutils.AddRoomRequest;
 import itmo.localpiper.backend.dto.response.HoldableResultResponse;
 import itmo.localpiper.backend.dto.response.OperationResultResponse;
+import itmo.localpiper.backend.exceptions.RoleViolationException;
+import itmo.localpiper.backend.model.House;
 import itmo.localpiper.backend.model.Room;
 import itmo.localpiper.backend.model.User;
+import itmo.localpiper.backend.model.UserHouseRel;
+import itmo.localpiper.backend.repository.FloorRepository;
+import itmo.localpiper.backend.repository.UserHouseRelRepository;
 import itmo.localpiper.backend.repository.UserRepository;
 import itmo.localpiper.backend.service.entity.RoomService;
 import itmo.localpiper.backend.service.transactional.TransactionalCreateRecursiveEntityService;
+import itmo.localpiper.backend.util.enums.HouseOwnership;
 import itmo.localpiper.backend.util.enums.ProcessingStatus;
 
 @Service
@@ -24,17 +32,28 @@ public class AddRoomProcessorService extends AbstractProcessor<Pair<String,AddRo
     private UserRepository userRepository;
 
     @Autowired
+    private FloorRepository floorRepository;
+
+    @Autowired
+    private UserHouseRelRepository uhrRepository;
+
+    @Autowired
     private TransactionalCreateRecursiveEntityService tcres;
+
 
     @Override
     protected Object send(Pair<String,AddRoomRequest> request) {
         String email = request.getFirst();
         String name = request.getSecond().getName();
         Long floorId = request.getSecond().getFloorId();
+
+        User user = userRepository.findByEmail(email).get();
         if (floorId == null) {
-            User user = userRepository.findByEmail(email).get();
             return tcres.createRoom(user, name);
         }
+        House house = floorRepository.findById(floorId).get().getHouse();
+        Optional<UserHouseRel> maybeUhr = uhrRepository.findByUserAndHouse(user, house);
+        if (maybeUhr.isEmpty() || maybeUhr.get().getRole() == HouseOwnership.GUEST) throw new RoleViolationException("Can't add room - permission denied");
         return roomService.create(name, floorId);
     }
 
